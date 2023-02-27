7e40933a2b1f2a4227cb52bed3302a580cb0362f
aleclindner <aleclindner@gmail.com>
2023-02-26 20:18:20 -0800

    add distribution

--- core/src/com/purple/PokemonPurple.java
+++ core/src/com/purple/PokemonPurple.java
@@ -12,7 +12,7 @@ public class PokemonPurple extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("squirtle/front.png");
 	}
 
 	@Override
